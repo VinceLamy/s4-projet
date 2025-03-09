5d074ea7244eac6015f0e0f9cdd66b7173bc8089
arthu <arthurolivier.fortin@gmail.com>
2025-03-09 11:01:48 -0400

    ...

--- vitis/c_code/lib/map.h
+++ vitis/c_code/lib/map.h
@@ -9,12 +9,14 @@ struct Map
     int rows;
     int cols;
     struct Cell*** matrix;
+
+    void (*moveViewport)(enum Direction);
 };
 
 // Déclaration des fonctions
 struct Map* __init_Map__();
 void __free_Map__(struct Map* map);
 void __str_Map__(struct Map* map);
-void send_Map(struct Map* map, void (*sendFunction)(void* data, size_t size)); //TODO
+void moveViewport(enum Direction);
 
 #endif 
